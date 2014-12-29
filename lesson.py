28aa1602067e8c836f0fd7ac9267e622c5c6e0f9
pauelstv <pauelstv@gmail.com>
2014-12-29 18:44:27 +0300

    Initial commit

---
+++ lesson.py
@@ -0,0 +1,13 @@
+__author__ = 'pauelstv'
+# program check IF ELIF ELSE construction
+exit_phrase = ""
+passphrase = ""
+
+#while exit_phrase != "exit":
+passphrase = input("Please login:")
+if passphrase == "pauelstv":
+    print("hello pauelstv")
+#    elif passphrase == "so":
+#        print("Hello So")
+#    else:
+#        print("user not found. exit")
