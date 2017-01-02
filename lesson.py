30ff67b45baa85f7e1580f6a9c408b5dd503f50f
pauelstv <pauelstv@gmail.com>
2017-01-03 02:48:50 +0300

    2017 test

--- lesson.py
+++ lesson.py
@@ -4,11 +4,11 @@ __author__ = 'pauelstv'
 exit_phrase = ""
 passphrase = ""
 
-#while exit_phrase != "exit":
+while exit_phrase != "exit":
 passphrase = input("Please login:")
 if passphrase == "pauelstv":
     print("hello pauelstv")
-#    elif passphrase == "so":
-#        print("Hello So")
-#    else:
-#        print("user not found. exit")
+    elif passphrase == "so":
+        print("Hello So")
+    else:
+        print("user not found. exit")
