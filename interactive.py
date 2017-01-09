7745cd09f41984d8fb6b6ecfaa2a50a7f6f7ec0d
pauelstv <pauelstv@gmail.com>
2017-01-09 11:50:33 +0300

    Lessons 2017

--- interactive.py
+++ interactive.py
@@ -1,13 +1,17 @@
 import os
 import platform
 import sys
+def getsysteminfo():
+    print(os.getcwd())
+    print(platform.processor())
+    print(sys.platform)
+    print(sys.getwindowsversion())
+    print(os.getpid())
+    print(os.getlogin())
 
-print(os.getcwd())
-print(platform.processor())
-print(sys.platform)
-print(sys.getwindowsversion())
-print(os.getpid())
-print(os.getlogin())
+getsysteminfo()
+
+print("Press any key to exit...")
 kek=input()
 
 
