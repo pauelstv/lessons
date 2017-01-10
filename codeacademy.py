5e01377a1ce63a69d3a5cdd1a4cdfb3f5e8465b7
pauelstv <pauelstv@gmail.com>
2017-01-10 15:50:04 +0300

    Lessons 2017

---
+++ codeacademy.py
@@ -0,0 +1,15 @@
+#строковые методы
+pi = 3.15
+print str(pi)
+
+"""Методы, которые используют точечную нотацию работают только со строками.
+С другой стороны, Len () и Str () может работать и на других типах данных.
+"""
+ministry = "The Ministry of Silly Walks"
+print len(ministry)
+print (ministry.upper())
+
+#конкатенация строк
+print ("Spam " + "and " + "eggs")
+#объединение строки с чем-то что не является строкой
+print "The value of pi is around " + str(3.14)
