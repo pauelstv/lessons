edbfa57994099780fb825469302d304cbd51b213
pauelstv <pauelstv@gmail.com>
2017-01-10 16:52:10 +0300

    Lessons 2017

--- codeacademy.py
+++ codeacademy.py
@@ -6,27 +6,30 @@ print (str(pi))
 С другой стороны, Len () и Str () может работать и на других типах данных.
 """
 ministry = "The Ministry of Silly Walks"
-print (len(ministry))
-print (ministry.upper())
+print(len(ministry))
+print(ministry.upper())
 
 #конкатенация строк
-print ("Spam " + "and " + "eggs")
+print("Spam " + "and " + "eggs")
 #объединение строки с чем-то что не является строкой
-print ("The value of pi is around " + str(3.14))
+print("The value of pi is around " + str(3.14))
 #форматирование строк. Если вы хотите напечатать переменную со строкой, есть лучший способ, чем конкатенация строк вместе.
 name = "Mike"
-print ("Hello %s" % (name))
+print("Hello %s" % (name))
 string_1 = "Camelot"
 string_2 = "place"
-print ("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
 #форматирование строк
-name = input("What is your name?")
+"""name = input("What is your name?")
 quest = input("What is your quest?")
 color = input("What is your favorite color?")
-
-print ("Ah, so your name is %s, your quest is %s, " \
-"and your favorite color is %s." % (name, quest, color))
+print("Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color))"""
 
 ###################### Date and time library
 from datetime import datetime
-
+now = datetime.now()
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
