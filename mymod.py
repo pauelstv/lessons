1c7254d2f572a2eccfdcc6bea5278cc25f81987a
pauelstv <pauelstv@gmail.com>
2017-01-30 15:53:05 +0300

    Lessons 2017

--- mymod.py
+++ mymod.py
@@ -1,6 +1,6 @@
 title = "azaza kek lol"
 
-a = [1, 2, 3]
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 b = a
 b[0] = 4
 print("A=", a)
@@ -13,4 +13,11 @@ print('Первые [8]: ', s[:8])
 print('Последние 8: ', s[8:-1])
 print('Каждый второй: ', s[::2])
 print('Наоборот: ', s[::-1])
+
+for num in a:
+    print(a[num], end=" ")
+
+for num in a:
+    print(a[num])
+
 
