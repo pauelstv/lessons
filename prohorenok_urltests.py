810728cfe3ca9ec5c126ca05540109064b2c06ca
pauelstv <pauelstv@gmail.com>
2017-02-08 17:40:38 +0300

    Lessons 2017

--- prohorenok_urltests.py
+++ prohorenok_urltests.py
@@ -1,8 +1,8 @@
 from urllib.parse import urlparse
 
 urladdr = urlparse("https://azaza.ru/kekhuypidor/")
-#print(urladdr)
-# преобразовываем в кортеж
+# print(urladdr)
+# преобразовываем результат в кортеж
 url = tuple(urladdr)
 print("Proto: ", url[0])
 print("Domain: ", url[1])
