15c9789a536bcca0b69e9773e0e07986c551a25b
pauelstv <pauelstv@gmail.com>
2017-02-09 13:43:11 +0300

    Lessons 2017

--- prohorenok_urltests.py
+++ prohorenok_urltests.py
@@ -12,3 +12,16 @@ print("Domain: ", url[1])
 print("Path: ", url[2])
 print("URL join:", urljoin("http://zlobatrix.ru/h1/h1/h1/1111test.php", "../../../file.php"))
 
+# ----------------------- HTTP communication ------------------------------------------------
+from http.client import HTTPConnection
+conn2 = HTTPConnection("telros-telecom.lan", 80)
+conn3 = HTTPConnection("telros-telecom.lan:80")
+
+conn1 = HTTPConnection("telros-telecom.lan")
+conn1.request("GET", "/")
+result = conn1.getresponse()
+print(result.read().decode("cp1251"))
+conn1.close()
+
+
+
