12108a9b8d09304009322319f4c13434a50b679f
pauelstv <pauelstv@gmail.com>
2017-02-09 11:30:20 +0300

    Lessons 2017

--- prohorenok_urltests.py
+++ prohorenok_urltests.py
@@ -1,15 +1,14 @@
-#from urllib.parse import urlparse
+# from urllib.parse import urlparse
 from urllib.parse import *
 
-
 urladdr = urlparse("https://what.cd.ru/keys/")
-# print(urladdr)
+print(urladdr)
+print("Object attribute: netloc ->", urladdr.netloc)
 # преобразовываем результат в кортеж
 url = tuple(urladdr)
+# атрибуты объекта доступны по индексам
 print("Proto: ", url[0])
 print("Domain: ", url[1])
 print("Path: ", url[2])
-
-
-
+print("URL join:", urljoin("http://zlobatrix.ru/h1/h1/h1/1111test.php", "../../../file.php"))
 
