192808ff572bd077113423bd70f481136318a22b
pauelstv <pauelstv@gmail.com>
2017-02-09 14:42:33 +0300

    Lessons 2017

--- prohorenok_urltests.py
+++ prohorenok_urltests.py
@@ -18,9 +18,30 @@ conn2 = HTTPConnection("telros-telecom.lan", 80)
 conn3 = HTTPConnection("telros-telecom.lan:80")
 
 conn1 = HTTPConnection("telros-telecom.lan")
-conn1.request("GET", "/")
+conn1.request("GET", "/drupal/")
 result = conn1.getresponse()
-print(result.read().decode("cp1251"))
+# print(result.read().decode("utf-8"))  # вывод HTML-кода
+urlheaders = result.getheaders()
+print(urlheaders[0], urlheaders[1])  # доуступно по индексам
+urldict = dict(urlheaders)
+print("Server headers:", urldict)
+print("Ключи словаря -> ", urldict.keys())
+print("Content-Type -> ", urldict['Content-Type'])
+print("Set-Cookie -> ", urldict['Set-Cookie'])
+print("Expires -> ", urldict['Expires'])
+print("Server -> ", urldict['Server'])
+print("Transfer-Encoding -> ", urldict['Transfer-Encoding'])
+print("Last-Modified -> ", urldict['Last-Modified'])
+print("ETag -> ", urldict['ETag'])
+print("Cache-Control -> ", urldict['Cache-Control'])
+print("Date -> ", urldict['Date'])
+
+
+
+
+
+
+
 conn1.close()
 
 
