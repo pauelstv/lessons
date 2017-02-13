5423f4687c0e95157cf595447082982a0da29c8d
pauelstv <pauelstv@gmail.com>
2017-02-13 12:03:40 +0300

    learning process 2017

--- temp.py
+++ temp.py
@@ -1,9 +1,19 @@
 import random
 for num in range(1):
     if random.random() >= 0.5:
-        print("Го срать")
+        print("Го куреть11")
     else:
-        print("Надо подождать со сраньем")
+        print("Куреву еще не время!")
+
+
+lottery = [99, 38, 11, 54, 55, 87]
+print("Not sorted: ", lottery)
+lottery.sort()
+print("Sorted:     ", lottery)
+
+
+
+
 
 
 
